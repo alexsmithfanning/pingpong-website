e072196ff3f6d0713fd388ebd54837a325876be9
Clarkba <clarkba@usd231.com>
2016-11-25 14:03:45 -0600

    Collapsable nav bar for mobile

---
+++ javascript/navcollapse.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+function expandnav() {
+	if(document.getElementById("nav").style.display = "none") {
+		document.getElementById("nav").style.display = "block";
+		document.getElementById("closenav").style.display = "block";
+	}
+}
+function collapsenav() {
+	if(document.getElementById("nav").style.display = "block") {
+		document.getElementById("nav").style.display = "none";
+		document.getElementById("closenav").style.display = "none";
+	}
+}
