920fbef238ac1b62877e90d9b39f32b3b69bd7c6
Clarkba <clarkba@usd231.com>
2016-11-24 22:41:22 -0600

    Gallery started

---
+++ javascript/gallery.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+function imageopener() {
+	document.getElementById("videowrap").style.display = "none";
+	document.getElementById("imagewrap").style.display = "block";
+}
+function videoopener() {
+	document.getElementById("imagewrap").style.display = "none";
+	document.getElementById("videowrap").style.display = "block";	
+}
