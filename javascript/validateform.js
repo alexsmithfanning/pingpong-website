727eaad7fb9c9085b099da088412c40d04602cc1
Clarkba <clarkba@usd231.com>
2016-11-28 17:57:58 -0600

    Contact form validation and mobile styling

---
+++ javascript/validateform.js
@@ -0,0 +1,17 @@
+function validateForm() {
+    var x = document.forms["contact"]["name"].value;
+	var y = document.forms["contact"]["email"].value;
+	var z = document.forms["contact"]["subject"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+	if (y == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+	if (z == "") {
+        alert("Subject must be filled out");
+        return false;
+    }
+}
