bdf5f1224efef6e7776e6585be415af48afe0df7
Clarkba <clarkba@usd231.com>
2016-12-02 09:01:00 -0600

    Create form.php

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+		
+		
+			if(isset($_REQUEST["submit"]))
+			{
+				$to = "henleyat@usd231.com";
+				$subject = $_REQUEST["subject"];
+				$message = $_REQUEST["message"];
+				$email = $_REQUEST["email"];
+				$name = $_REQUEST["name"];
+				$from = $name." ".$email;
+				mail($to, $subject, $message, $from, $name);
+			}
+    ?>
