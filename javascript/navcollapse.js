c3c65cb9014b8d70a99ede29a1368e4f939848a1
Clarkba <clarkba@usd231.com>
2016-12-07 18:56:34 -0600

    HOPEFULLY Final touches

--- javascript/navcollapse.js
+++ javascript/navcollapse.js
@@ -5,6 +5,7 @@ function expandnav() {
 		document.getElementById("nav").style.display = "block";
 		document.getElementById("closenav").style.display = "block";
 		document.getElementById("opennav").style.display = "none";
+		document.getElementById("uhoh").style.top = "65%";
 	}
 }
 
@@ -13,5 +14,6 @@ function collapsenav() {
 		document.getElementById("nav").style.display = "none";
 		document.getElementById("closenav").style.display = "none";
 		document.getElementById("opennav").style.display = "block";
+		document.getElementById("uhoh").style.top = "25%";
 	}
 }
